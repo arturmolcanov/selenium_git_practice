fbd6f06de4355ffcc6f75fa4da392ae8325c47f1
arturmolcanov <molcanov.artur@gmail.com>
2021-10-15 15:22:10 +0300

    new branch test commit

--- TestData/HomePageData.py
+++ TestData/HomePageData.py
@@ -1,3 +1,5 @@
 class HomePageData:
     test_HomePage_data = [{"firstname": "Artur", "email": "tests@test", "gender": "Male"},
-                         {"firstname": "Artur2", "email": "tests@test2", "gender": "Male"}]
+                         {"firstname": "Artur2", "email": "tests@test2", "gender": "Male"},
+                         {"firstname": "Artur3", "email": "tests@test3", "gender": "Female"},
+                         {"firstname": "Artur4", "email": "tests@test4", "gender": "Female"}]
